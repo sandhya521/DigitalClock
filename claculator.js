7987483bb23c416fc42e8d20c16a517f338a1ec7
sandhya521 <138872968+sandhya521@users.noreply.github.com>
2023-07-07 16:06:11 +0530

    Add files via upload

---
+++ claculator.js
@@ -0,0 +1,11 @@
+var clickSound=new Audio()
+clickSound.src="./click.mp3"
+clickSound1.src="./click1.mp3"
+function clickAudio()
+{
+    clickSound.play()
+}
+function clickAudio1()
+{
+    clickSound.play()
+}
